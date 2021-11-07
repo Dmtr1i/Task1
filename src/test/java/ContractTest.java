df0ba76d460c67c4708b54c3fab9252a9f7bb40a
Dmtr1i <dmitry1klimenko@mail.ru>
2021-11-07 10:14:52 +0300

    add PersonTest

---
+++ src/test/java/ContractTest.java
@@ -0,0 +1,46 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ContractTest {
+
+    @Test
+    public void getId() {
+    }
+
+    @Test
+    public void getStartContract() {
+    }
+
+    @Test
+    public void getEndContract() {
+    }
+
+    @Test
+    public void getNumber() {
+    }
+
+    @Test
+    public void getOwner() {
+    }
+
+    @Test
+    public void setId() {
+    }
+
+    @Test
+    public void setStartContract() {
+    }
+
+    @Test
+    public void setEndContract() {
+    }
+
+    @Test
+    public void setNumber() {
+    }
+
+    @Test
+    public void setOwner() {
+    }
+}
---
+++ src/test/java/PersonTest.java
@@ -0,0 +1,127 @@
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.Assert.*;
+
+public class PersonTest {
+
+    @Test
+    public void getId() {
+        Person boi = new Person(0, "Klimenko Dmitry Sergeevitch", LocalDate.of(2001, 8, 24), "male", 152634, 1618);
+        long actual = boi.getId();
+        long expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getName() {
+        Person boi = new Person(0, "Klimenko Dmitry Sergeevitch", LocalDate.of(2001, 8, 24), "male", 152634, 1618);
+        String actual = boi.getName();
+        String expected = "Klimenko Dmitry Sergeevitch";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getBirthDate() {
+        Person boi = new Person(0, "Klimenko Dmitry Sergeevitch", LocalDate.of(2001, 8, 24), "male", 152634, 1618);
+        LocalDate actual = boi.getBirthDate();
+        LocalDate expected = LocalDate.of(2001, 8, 24);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getGender() {
+        Person boi = new Person(0, "Klimenko Dmitry Sergeevitch", LocalDate.of(2001, 8, 24), "male", 152634, 1618);
+        String actual = boi.getGender();
+        String expected = "male";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getPasportSeries() {
+        Person boi = new Person(0, "Klimenko Dmitry Sergeevitch", LocalDate.of(2001, 8, 24), "male", 152634, 1618);
+        int actual = boi.getPasportSeries();
+        int expected = 1618;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getPasportNumber() {
+        Person boi = new Person(0, "Klimenko Dmitry Sergeevitch", LocalDate.of(2001, 8, 24), "male", 152634, 1618);
+        int actual = boi.getPasportNumber();
+        int expected = 152634;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getAge() {
+        Person boi = new Person(0, "Klimenko Dmitry Sergeevitch", LocalDate.of(2001, 8, 24), "male", 152634, 1618);
+        int actual = boi.getAge();
+        int expected = 20;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setId() {
+        Person boi = new Person(0, "Klimenko Dmitry Sergeevitch", LocalDate.of(2001, 8, 24), "male", 152634, 1618);
+        boi.setId(20);
+        long actual = boi.getId();
+        long expected = 20;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setName() {
+        Person boi = new Person(0, "Klimenko Dmitry Sergeevitch", LocalDate.of(2001, 8, 24), "male", 152634, 1618);
+        boi.setName("Semenov Petr Ivanovitch");
+        String actual = boi.getName();
+        String expected = "Semenov Petr Ivanovitch";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setBirthDate() {
+        Person boi = new Person(0, "Klimenko Dmitry Sergeevitch", LocalDate.of(2001, 8, 24), "male", 152634, 1618);
+        boi.setBirthDate(LocalDate.of(2002, 6, 13));
+        LocalDate actual = boi.getBirthDate();
+        LocalDate expected = LocalDate.of(2002, 6, 13);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setGender() {
+        Person boi = new Person(0, "Klimenko Dmitry Sergeevitch", LocalDate.of(2001, 8, 24), "male", 152634, 1618);
+        boi.setGender("Male");
+        String actual = boi.getGender();
+        String expected = "Male";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setPasportNumber() {
+        Person boi = new Person(0, "Klimenko Dmitry Sergeevitch", LocalDate.of(2001, 8, 24), "male", 152634, 1618);
+        boi.setPasportNumber(205613);
+        int actual = boi.getPasportNumber();
+        int expected = 205613;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setPasportSeries() {
+        Person boi = new Person(0, "Klimenko Dmitry Sergeevitch", LocalDate.of(2001, 8, 24), "male", 152634, 1618);
+        boi.setPasportSeries(1826);
+        long actual = boi.getPasportSeries();
+        long expected = 1826;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setAge() {
+        Person boi = new Person(0, "Klimenko Dmitry Sergeevitch", LocalDate.of(2001, 8, 24), "male", 152634, 1618);
+        boi.setAge(46);
+        int actual = boi.getAge();
+        int expected = 46;
+        assertEquals(expected, actual);
+    }
+}
