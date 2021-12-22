9ced54857cfecc004cba31a736924f45f1a4707b
Dmtr1i_laptop <dmitry1klimenko@mail.ru>
2021-12-23 00:15:14 +0300

    add read owner information

--- src/main/java/CSVWorker/CSVWorker.java
+++ src/main/java/CSVWorker/CSVWorker.java
@@ -8,6 +8,7 @@ import com.opencsv.exceptions.CsvValidationException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.function.Predicate;
 
 public class CSVWorker <T extends Contract> {
@@ -26,6 +27,21 @@ public class CSVWorker <T extends Contract> {
             while ((data = reader.readNext()) != null) {
                 for (int i = 0; i < data.length; i++) System.out.print(data[i]);
                 System.out.println();
+                if (data[0].contains("InternetContract")) {
+                    //Person owner;
+                    int ownerId = Integer.parseInt(data[5].replaceAll("\\s", ""));
+                    String ownerName = data[6].replaceAll("^\\s", "");
+                    String dateTemp = data[7].replaceAll("\\s", "");
+                    String[] dateTemp2 = dateTemp.split("\\.");
+                    int ownerBirthDateDay = Integer.parseInt(dateTemp2[0].replaceAll("\\s", ""));
+                    int ownerBirthDateMonth = Integer.parseInt(dateTemp2[1].replaceAll("\\s", ""));
+                    int ownerBirthDateYear = Integer.parseInt(dateTemp2[2].replaceAll("\\s", ""));
+                    String ownerGender = data[8].replaceAll("\\s", "");
+                    int ownerPasSeries = Integer.parseInt(data[9].replaceAll("\\s", ""));
+                    int ownerPasNumber = Integer.parseInt(data[10].replaceAll("\\s", ""));
+                    Person me = new Person(ownerId, ownerName, LocalDate.of(ownerBirthDateYear, ownerBirthDateMonth, ownerBirthDateDay), ownerGender, ownerPasNumber, ownerPasSeries);
+                    System.out.println(me.toString());
+                }
             }
         } catch (IOException ex) {
             System.out.println("Error: " + ex);
@@ -48,7 +64,8 @@ public class CSVWorker <T extends Contract> {
                 data = temp.toString().split(",");
                 String[] data2 = new String[data.length + 1];
                 data2[0] = temp.getClass().getSimpleName();
-                for (int j = 0; j < data.length; j++) data2[j + 1] = data[j];
+                data2[1] = data[0].format("\s%s", data[0]);
+                for (int j = 1; j < data.length; j++) data2[j + 1] = data[j];
                 writer.writeNext(data2);
             }
             writer.close();
